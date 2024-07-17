bc31ab2d860e7dff232f386f884a8f9242fbb410
shahfawadkhan <you@sfawadkhan92@gmail.com>
2024-07-17 04:14:08 -0700

    js problems

---
+++ script.js
@@ -0,0 +1,66 @@
+
+//1 => table
+// var no = +prompt();
+
+// for (var i = 1 ; i<=10 ; i++){
+//     console.log(`${i} * ${no} = ${i*no}`)
+// }
+
+//2 => sum of array
+
+// const array = [];
+
+// for (var i = 0 ; i<5 ; i++){
+//     const userNo = +prompt("lekhoo");
+//     array.push(userNo);
+// }
+// var sum = 0;
+// for ( var i = 0 ; i < array.length ; i++){
+//     sum = sum + array[i];
+// }
+// console.log(sum);
+
+// console.log(array);
+
+//3 => finding max number
+
+// const array = [];
+
+// for (var i = 0 ; i<5 ; i++){
+//     const userNo = +prompt("lekhoo");
+//     array.push(userNo);
+// }
+// var max = array[0];
+// for (var i = 0 ; i < array.length ; i++){
+//     if(array[i] > max){
+//         max = array[i];
+//     }
+// }
+// console.log(max)
+
+//4 => Reversing string
+
+// var string = prompt("your name");
+// var result = "";
+// var flag;
+
+// for (var i = string.length - 1 ; i>= 0 ; i--){
+//     if(string!== null){
+//         result += string[i]
+//         flag = true;
+//     }
+// }
+// if(flag){
+//     console.log(result);
+// }else{
+//     console.log("inavalid")
+// }
+
+//5 => finding factorial
+
+// var no = +prompt();
+// var factorial = 1;
+// for (var i = no ; i>0 ; i--){
+//      factorial *= i;
+// }
+// console.log(factorial);
